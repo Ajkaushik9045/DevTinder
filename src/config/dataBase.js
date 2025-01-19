5a7548fd66f833052f57f472c6f3e313589706df
Ajkaushik9045 <staraj89@gmail.com>
2025-01-19 19:38:32 +0530

    Chnaging Srv String

--- src/config/dataBase.js
+++ src/config/dataBase.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   await mongoose.connect(
-    "mongodb+srv://amitdhiman5086:8MTOBl1mUSV9IYA7@cluster0.vzohn.mongodb.net/devTinder"
+    "mongodb+srv://brandbridge45:34@98Zftc6rQLnH@cluster0.l5nwadx.mongodb.net/devTinder"
   );
+  
 };
+
+
 
 module.exports = connectDb;
