fc60b2b17fd8f7f6f0d56487fb211b338c49ffda
Anuj kaushik <104207655+Ajkaushik9045@users.noreply.github.com>
2025-02-09 18:26:32 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://brandbridge45:Anuj123@cluster0.l5nwadx.mongodb.net/DevTinder";
+
 
 const connectDb = async () => {
     await mongoose.connect(uri);
